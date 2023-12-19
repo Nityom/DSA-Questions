3d5cf5058d0e8606bc2aefaf164fda1522486072
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2023-12-19 12:36:12 +0530

    Time: 74 ms (36.16%) | Memory: 50.2 MB (45.35%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,15 @@
+function maxSubArray(nums) {
+    if (nums.length === 0) {
+        return 0;
+    }
+
+    let maxSum = nums[0];
+    let currentSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        currentSum = Math.max(nums[i], currentSum + nums[i]);
+        maxSum = Math.max(maxSum, currentSum);
+    }
+
+    return maxSum;
+}
