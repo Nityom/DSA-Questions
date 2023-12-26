a682574354bdf77d458eedf4957640e9b36cc210
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2023-12-26 09:27:29 +0530

    Time: 49 ms (94.40%) | Memory: 43.9 MB (67.74%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,31 @@
+class ListNode {
+    constructor(val, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+function mergeTwoLists(l1, l2) {
+    const dummy = new ListNode();
+    let current = dummy;
+
+    while (l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            current.next = l1;
+            l1 = l1.next;
+        } else {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    if (l1 !== null) {
+        current.next = l1;
+    } else {
+        current.next = l2;
+    }
+
+    return dummy.next;
+}
+
