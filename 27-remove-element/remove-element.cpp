c96b25640a148b0c713bbe2f391bf547b0478c59
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2024-08-17 13:24:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (23.65%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int write = 0;
+        int pointer = 0;
 
         for(int i=0; i<nums.size();i++){
             if(nums[i]!= val){
-                nums[write] = nums[i];
-                write++;
+                nums[pointer] = nums[i];
+                pointer++;
 
             }
         }
-        return write ;
+        return pointer ;
     }
 };
