4b3378ce57179e712a46664639a04351cbdcb871
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2024-08-17 12:52:22 +0530

    Time: 7 ms (43.71%) | Memory: 24.1 MB (14.39%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,19 +1,32 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    int countPairs(vector<int>& nums, int target) {
-
-        int count =0;
+    int countPairs(std::vector<int>& nums, int target) {
+        int count = 0;
         int n = nums.size();
 
-        for(int i = 0 ;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]+nums[j]<target){
-                    count++;
-              
-                }
+ 
+        std::sort(nums.begin(), nums.end());
+
+
+        int left = 0;
+        int right = n - 1;
+
+    
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+
+                count += (right - left);
+        
+                ++left;
+            } else {
+          
+                --right;
             }
         }
-              return count;
-        
+
+        return count;
     }
 };
