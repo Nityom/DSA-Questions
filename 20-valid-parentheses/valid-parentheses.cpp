c9a5d746d6c5922e7a90cbaf4077166e6eeb727b
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2025-02-12 20:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (62.00%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {  
+        stack<char> st;
+        unordered_map<char, char> pairs = {{')', '('}, {'}', '{'}, {']', '['}};
+        
+        for (char c : s) {   
+            if (pairs.count(c)) { 
+                if (st.empty() || st.top() != pairs[c]) {
+                    return false;
+                }
+                st.pop(); 
+            } else {
+                st.push(c); 
+            }
+        }
+        
+        return st.empty(); 
+    }
+};
