db91c717dbd1aa72be3b2a2c517aa9ef755b7d67
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2025-08-20 10:59:20 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (80.02%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,22 +1,20 @@
 class Solution {
+    int helper(vector<int>&nums,int left,int right,int target){
+        if(left>right) return -1;
+        int mid = (left+right)/2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid]>target){
+              return helper(nums,left,mid-1,target);
+        }
+        else{
+             return  helper(nums,mid+1,right,target);
+        }
+
+    }
 public:
     int search(vector<int>& nums, int target) {
-
-        int left=0;
-        int right = nums.size()-1;
-        
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target)
-            return mid;
-            else if(nums[mid]<target)
-            left = mid+1;
-            else
-            right = mid-1;
-        }
-        return -1;
-
-    
+    int left =0;
+    int right = nums.size()-1;
+    return helper(nums,0,nums.size()-1,target);
     }
- 
 };
