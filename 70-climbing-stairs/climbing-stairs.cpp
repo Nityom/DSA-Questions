2ff6ce907f7e5272c9441c9b2d7e8e3d7e1f6095
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2025-09-16 22:43:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (18.66%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    int helper(int index,vector<int>& dp){
+    int helper(int index, vector<int>& dp){
         if(index == 0 || index == 1){
             return 1;
         }
@@ -7,13 +7,12 @@ class Solution {
         if(dp[index]!=-1){
             return dp[index];
         }
-
-        dp[index] = helper(index - 1,dp) + helper(index - 2,dp);
-        return dp[index];
+  
+        return dp[index]= helper(index-1,dp) + helper(index-2,dp);
     }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 1 , -1);
-        return helper(n,dp);
+        vector<int>dp(n+1,-1);
+       return helper(n,dp);
     }
 };
