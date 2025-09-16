5f529412346dbcdbc3d71c5481a7faeac789514b
Nityom Tikhe <112824495+Nityom@users.noreply.github.com>
2025-09-16 23:09:54 +0530

    Time: 174 ms (20.96%) | Memory: 110.4 MB (46.64%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,30 +1,40 @@
 class Solution {
-    bool helper(int index, int target, vector<int>& nums, vector<vector<int>>& dp) {
-        if (target == 0) return true;           // found a subset
-        if (index >= nums.size() || target < 0) return false;
 
-        if(dp[index][target] != -1){
-            return dp[index][target];
-        }
+    int helper(int index, int target, vector<int>& nums, vector<vector<int>>& dp){   
+      
+     if(target == 0) return true;
 
-        // include or exclude current element
-        bool take = helper(index + 1, target - nums[index], nums,dp);
-        bool notTake = helper(index + 1, target, nums,dp);
+     if(index>=nums.size() || target < 0) return false;
 
-        return dp[index][target] = take || notTake;
+     if(dp[index][target] != -1){
+        return dp[index][target];
+     }
+
+    bool  take = helper(index+1,  target- nums[index],nums,dp);
+    bool nottake = helper(index+1, target,nums,dp);
+
+    return dp[index][target] = take || nottake;
+
     }
-
 public:
     bool canPartition(vector<int>& nums) {
-         
-        int sum = 0;
-        for (int num : nums) sum += num;
+     
+        int n = nums.size();
+        int sum =0;
 
-        if (sum % 2 != 0) return false;  // odd sum can't be split equally
-        int target = sum / 2;
+        for(int i =0;i<n;i++){
+            sum += nums[i];
+        }
 
-        vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,-1));
+       int mid = sum/2;
 
-        return helper(0, target, nums,dp);
+       if(sum%2!=0){
+  return false;
+       }
+      int target = sum/2;
+     vector<vector<int>> dp(n, vector<int>(target+1,-1));
+      
+      return helper(0,target,nums,dp);
+
     }
 };
